f2b6e254064c8f2fe8579b329ba4d77d40a9dad1
madhuyuva <madhuyuvasoft344@gmail.com>
2022-08-17 16:51:46 +0530

    added login file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Register from "./Components/Register";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Login from "./Components/Login";
+import Dashboard from "./Components/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <Register />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+
+      </Routes>
+    </BrowserRouter>
+    
     </div>
   );
 }
